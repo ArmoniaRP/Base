667e5d1d2030e9ba8e38f8ad1075491c17dc303c
Raphael-Clenet <114576173+Raphael-Clenet@users.noreply.github.com>
2025-01-13 19:13:59 +0100

    fix: remove useless import

--- staff_menu/fxmanifest.lua
+++ staff_menu/fxmanifest.lua
@@ -7,15 +7,6 @@ version '1.0.0'
 
 -- Fichiers côté client et serveur
 client_scripts {
-    '@RageUI/RMenu.lua',
-    '@RageUI/menu/RageUI.lua',
-    '@RageUI/menu/Menu.lua',
-    '@RageUI/menu/MenuController.lua',
-    '@RageUI/components/*.lua',
-    '@RageUI/menu/elements/*.lua',
-    '@RageUI/menu/items/*.lua',
-    '@RageUI/menu/panels/*.lua',
-    '@RageUI/menu/windows/*.lua',
     '@es_extended/locale.lua',
     'client.lua',
     'menu.lua'
